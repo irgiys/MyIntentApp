0a3ac367e9432c6832c671a2d76fe54ef7bfdcac
irgiys <irgiyansy@gmail.com>
2023-11-23 21:17:36 +0700

    Parcelize

--- app/src/main/java/com/dicoding/myintentapp/MoveWithObjectActivity.kt
+++ app/src/main/java/com/dicoding/myintentapp/MoveWithObjectActivity.kt
@@ -14,6 +14,7 @@ class MoveWithObjectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_move_with_object)
 
         val tvObject: TextView = findViewById(R.id.tv_object_received)
+
         val person = if(Build.VERSION.SDK_INT >= 33){
             intent.getParcelableExtra<Person>(EXTRA_PERSON, Person::class.java)
         } else {
